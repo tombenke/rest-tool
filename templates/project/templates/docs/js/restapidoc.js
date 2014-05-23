bd65f51c0cded14e30e0d2ef66ecffb893570265
Tamás Benke <tombenke@gmail.com>
2014-05-23 18:50:39 +0200

    Some fine tuning is done on SASS classes and HTML templates Add icons/classes and functions to: expand / collapse blocks

--- templates/project/templates/docs/js/restapidoc.js
+++ templates/project/templates/docs/js/restapidoc.js
@@ -1,10 +1,18 @@
 jQuery(document).ready(function() {
 
     function toggleBlock() {
+        if( $(this).is('.toggle-down') ) {
+            $(this).toggleClass( "toggle-up", true );
+            $(this).toggleClass( "toggle-down", false );
+        } else {
+            $(this).toggleClass( "toggle-up", false );
+            $(this).toggleClass( "toggle-down", true );
+        }
         $(this).next().toggle(100);
     }
 
     function initEventHandlers() {
+        $('div.block-title').toggleClass( "toggle-down", true );
         $('div.block-title').next().toggle();
         $('div.block-title').bind('click', toggleBlock );
     }
