525fbb28a966185b7ad85d8ae7998cfe414195ae
Denes Laszlo <denes.laszlo.sp@lhsystems.com>
2014-06-14 13:01:48 +0200

    Improve documentroot handling
    Documentroot can be set as a list of paths too,
    and also as a simple string
    
    ----------------------
    branch [[master]]

--- templates/project/server/server.js
+++ templates/project/server/server.js
@@ -1,11 +1,12 @@
 #!/usr/bin/env node
+
 /*jshint node: true */
 "use strict";
 
-var fs = require( 'fs' );
-var path = require( 'path' );
-var express = require( 'express' );
-var request = require( 'request' );
+var fs = require('fs');
+var path = require('path');
+var express = require('express');
+var request = require('request');
 var api = require('./api.js');
 var monitoring = require('./monitoring.js');
 var services = require('rest-tool-common').services;
@@ -14,9 +15,9 @@ var proxy = require('./proxy');
 // Get configured
 var config = {};
 exports.config = config;
-if( process.argv.length >= 3 ) {
-    var baseConfig = require( path.resolve(__dirname, 'config.js'));
-    config = baseConfig.setEnvironment( process.argv[2] );
+if (process.argv.length >= 3) {
+    var baseConfig = require(path.resolve(__dirname, 'config.js'));
+    config = baseConfig.setEnvironment(process.argv[2]);
 } else {
     config = require(path.resolve(__dirname, './config.js')).parameters;
 }
@@ -29,41 +30,52 @@ var servicesConfig = services.getConfig();
 // console.log('rest-tool config:', servicesConfig);
 
 var server = module.exports = express();
-server.set('env', config.environment );
+server.set('env', config.environment);
 
 // Configure the middlewares
-server.configure( function() {
-        server.use( proxy(servicesConfig.serviceUrlPrefix, config.remoteServices, config.proxyException) );
-        server.use( express.bodyParser() );
-        server.use( express.compress() );
-        server.use( express.responseTime() );
-        server.use( express.methodOverride() );
-        server.use( express.cookieParser() );
-        server.use( express.session( {secret: 'keyboard cat'} ) );
-        server.use( server.router );
-        server.use( '/data', express.static( __dirname + '/' + '../data' ) );
-        server.use( '/docs', express.static( __dirname + '/' + '../docs' ) );
-        server.use( '/services', express.static( __dirname + '/../services' ) );
-        server.use( express.static( __dirname + '/' + config.documentRoot ) );
-    });
+server.configure(function() {
+    server.use(proxy(servicesConfig.serviceUrlPrefix, config.remoteServices, config.proxyException));
+    server.use(express.bodyParser());
+    server.use(express.compress());
+    server.use(express.responseTime());
+    server.use(express.methodOverride());
+    server.use(express.cookieParser());
+    server.use(express.session({
+        secret: 'keyboard cat'
+    }));
+    server.use(server.router);
+    server.use('/data', express.static(__dirname + '/' + '../data'));
+    server.use('/docs', express.static(__dirname + '/' + '../docs'));
+    server.use('/services', express.static(__dirname + '/../services'));
+    //server.use(express.static(__dirname + '/' + config.documentRoot));
 
-server.configure( 'development', function() {
-        server.use( express.errorHandler( {
-                    dumpExceptions: true,
-                    showStack: true
-                }));
-    });
+    if (Array.isArray(config.documentRoot)) {
+        config.documentRoot.forEach(function(e) {
+            server.use(express.static(__dirname + '/' + e));
+        });
+    } else {
+        server.use(express.static(__dirname + '/' + config.documentRoot));
+    }
 
-server.configure( 'production', function() {
-        server.use( express.errorHandler() );
-    });
+});
 
-function restrict( req, res, next ) {
+server.configure('development', function() {
+    server.use(express.errorHandler({
+        dumpExceptions: true,
+        showStack: true
+    }));
+});
+
+server.configure('production', function() {
+    server.use(express.errorHandler());
+});
+
+function restrict(req, res, next) {
     next();
 }
 
-function accessLogger( req, res, next ) {
-    console.log( req.method, req.url );
+function accessLogger(req, res, next) {
+    console.log(req.method, req.url);
     next();
 }
 
@@ -71,25 +83,25 @@ function accessLogger( req, res, next ) {
 server.all("*", accessLogger, restrict);
 
 function writeHeader(response) {
-    response.header( 'Content-Type', 'application/json' );
-    response.header( 'X-pmd-api-API-Version', servicesConfig.apiVersion );
+    response.header('Content-Type', 'application/json');
+    response.header('X-pmd-api-API-Version', servicesConfig.apiVersion);
 }
 exports.writeHeader = writeHeader;
 
 function writeResponse(response, content) {
     writeHeader(response);
-    response.write( JSON.stringify(content, null, '  ') );
-    response.end( '\n' );
+    response.write(JSON.stringify(content, null, '  '));
+    response.end('\n');
 }
 exports.writeResponse = writeResponse;
 
-var defaultServiceCall = function (request, response, serviceDesc) {
-    response.header( 'Content-Type', 'application/json' );
+var defaultServiceCall = function(request, response, serviceDesc) {
+    response.header('Content-Type', 'application/json');
     // TODO: Use Headers and Cookies from serviceDesc
-    writeResponse(response, services.getMockResponseBody(request.method, serviceDesc ) || serviceDesc);
+    writeResponse(response, services.getMockResponseBody(request.method, serviceDesc) || serviceDesc);
 };
 
-var reformatUrlPattern = function (uriTemplate) {
+var reformatUrlPattern = function(uriTemplate) {
     // TODO: Replace {parameter} to :parameter
     var resultPattern = uriTemplate.replace(/{/gi, ":").replace(/}/gi, "").toString();
     console.log(resultPattern);
@@ -100,17 +112,17 @@ var reformatUrlPattern = function (uriTemplate) {
 function registerServiceMethod(serviceDesc, method) {
     console.log('register service ' + method + ' ' + serviceDesc.uriTemplate);
     var methodDesc = serviceDesc.methods[method];
-    var implementation = eval( serviceDesc.methods[method].implementation ) || defaultServiceCall;
+    var implementation = eval(serviceDesc.methods[method].implementation) || defaultServiceCall;
     server[method.toLowerCase()](servicesConfig.serviceUrlPrefix + reformatUrlPattern(serviceDesc.uriTemplate), function(request, response) {
         implementation(request, response, serviceDesc);
     });
 }
 
-for ( var service in allServices ) {
-    if ( allServices.hasOwnProperty(service) ) {
+for (var service in allServices) {
+    if (allServices.hasOwnProperty(service)) {
         var serviceDesc = allServices[service];
-        for ( var method in allServices[service].methods ) {
-            if ( serviceDesc.methods.hasOwnProperty(method) ) {
+        for (var method in allServices[service].methods) {
+            if (serviceDesc.methods.hasOwnProperty(method)) {
                 registerServiceMethod(serviceDesc, method);
             }
         }
@@ -118,6 +130,6 @@ for ( var service in allServices ) {
 }
 
 // Start the server to listen
-server.listen( config.port );
-console.log( "Express server listening on port %d in %s mode",
-    config.port, server.settings.env );
+server.listen(config.port);
+console.log("Express server listening on port %d in %s mode",
+    config.port, server.settings.env);
